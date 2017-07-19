b6270b6ca9f0a7bda15ad1cbbc24ed5d597d46b7
刘宗宇 <zongyu.liu@foxmail.com>
2017-07-19 15:11:12 +0800

    vue 2.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# 概述
+最近学习vue2.0和elementUI的使用，在各种文档的帮助下，尝试编写了一个后台管理平台。
+目前数据采用mock.js模拟，比较简略。后续会进行细化并增加其他等功能。
+# 依赖项版本
+    "vue": "^2.1.0",
+    "vue-router": "^2.1.3", // vue.js官方路由
+    "axios": "^0.16.1",   // 官方已不再推荐使用vue-resource，如今推荐axios。
+    "element-ui": "^1.2.3", // 样式库
+    "mockjs": "^1.0.1-beta3", //模拟数据使用
+    
+# 特别提醒
+登录账号和密码都为admin
+ 
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/mock/data/user.js
@@ -0,0 +1,25 @@
+import Mock from 'mockjs';
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'admin',
+    password: '123456',
+    avatar: 'https://github.com/cinoliu/ceshi/blob/3621b9d304f0d1f5550ae51e1d853e040d7422ad/18730677.jpeg?raw=true',
+    name: 'CinoLiu'
+  }
+];
+
+const Users = [];
+
+//for (let i = 0; i < 86; i++) {
+//  Users.push(Mock.mock({
+//    id: Mock.Random.guid(),
+//    name: Mock.Random.cname(),
+//    addr: Mock.mock('@county(true)'),
+//    'age|18-60': 1,
+//    birth: Mock.Random.date(),
+//    sex: Mock.Random.integer(0, 1)
+//  }));
+//}
+
+export { LoginUsers, Users };
